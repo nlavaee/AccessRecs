0bbf5dea721a12771b9dd1c2feb2d46f4ce63ea7
nlavaee <nlavaee@umich.edu>
2019-11-17 22:02:02 -0500

    completely new guide + some nav fixes

---
+++ AccesRecs/AccesRecs/DropDown.swift
@@ -0,0 +1,9 @@
+//
+//  DropDown.swift
+//  AccesRecs
+//
+//  Created by Norin Lavaee on 11/13/19.
+//  Copyright © 2019 Norin Lavaee. All rights reserved.
+//
+
+import Foundation
---
+++ AccesRecs/AccesRecs/SettingsStepsView.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsSteps.swift
+//  AccesRecs
+//
+//  Created by Norin Lavaee on 11/17/19.
+//  Copyright © 2019 Norin Lavaee. All rights reserved.
+//
+
+import Foundation
